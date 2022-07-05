148408a569fc38f22a2ac147daec67656e8cf28d
matsuyami <mkadwaki@gmail.com>
2022-07-05 00:16:59 -0700

    cleaned up models

--- app/src/main/java/com/matsuyami/recipy/models/remote/Component.kt
+++ app/src/main/java/com/matsuyami/recipy/models/remote/Component.kt
@@ -1,20 +1,15 @@
 package com.recipy.models
 
-
 import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
-data class Component(
-    @SerializedName("extra_comment")
-    val extraComment: String,
+data class ComponentX(
     @SerializedName("id")
     val id: Int,
     @SerializedName("ingredient")
-    val ingredient: Ingredient,
-    @SerializedName("measurements")
-    val measurements: List<Measurement>,
+    val ingredient: IngredientX,
     @SerializedName("position")
     val position: Int,
     @SerializedName("raw_text")
     val rawText: String
-): Serializable
+) : Serializable
--- app/src/main/java/com/matsuyami/recipy/models/remote/Section.kt
+++ app/src/main/java/com/matsuyami/recipy/models/remote/Section.kt
@@ -1,14 +1,13 @@
 package com.recipy.models
 
-
 import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
-data class Section(
+data class SectionX(
     @SerializedName("components")
-    val components: List<Component>,
+    val components: List<ComponentX>,
     @SerializedName("name")
     val name: Any,
     @SerializedName("position")
     val position: Int
-): Serializable
+) : Serializable
